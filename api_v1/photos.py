74c164ea23e40d8e27ac71e0afeda35136ff928a
Yoon sang seok <63184031+amamov@users.noreply.github.com>
2020-11-11 22:19:38 +0900

    google 추가

--- api_v1/photos.py
+++ api_v1/photos.py
@@ -1,7 +1,7 @@
 import requests
 from flask import jsonify, request
 from api_v1 import api
-from scraper import NaverPhoto
+from scraper import NaverPhoto, GooglePhoto
 
 
 @api.route("/photos")
@@ -9,26 +9,16 @@ def photos():
     return jsonify(), 200
 
 
+# 스레드 분리해서 각 site마다 병렬로 사진 가져오기
 @api.route("/photos/<site>/<keyword>")
 def photos_search(site, keyword):
-    if site == "naver" or site == "NAVER":
+    if site == "naver" or site == "NAVER" or site == "Naver":
         photo = NaverPhoto(keyword)
+        photo.scrape()
+        return jsonify(photo.images), 200
+    elif site == "google" or site == "GOOGLE" or site == "Google":
+        photo = GooglePhoto(keyword)
         photo.scrape()
         return jsonify(photo.images), 200
     else:
         return jsonify(), 400
-
-
-# post 방식으로 데이터를 가져오자! (띄어쓰기 문제)
-# 네이버는 성인인증이 필요한 경우면 크롤링 불가능
-
-# @api.route("/photos/<site>", methods=['POST', 'GET'])
-# def photos_search(site):
-#     if request.method='POST':
-#         if site == "naver" or site == "NAVER":
-#             keyword=request.get_json()
-#             photo = NaverPhoto(keyword)
-#             photo.scrape()
-#             return jsonify(photo.images), 200
-#         else:
-#             return jsonify(), 400
--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@ import requests
 
 class NaverPhoto:
 
-    """ 네이버 사진 스크래핑 50장 """
+    """ 네이버 사진 스크래핑 50장 (네이버는 성인인증이 필요한 경우면 크롤링 불가능) """
 
     BASE_URL = "https://search.naver.com/search.naver?where=image&sm=tab_jum&query="
 
@@ -37,7 +37,45 @@ class NaverPhoto:
                     self.images["src"].append(img_src)
 
 
+class GooglePhoto:
+
+    """ 구글 사진 크롤링 """
+
+    BASE_URL = "https://www.google.com/search?q="
+    SEARCH_URL = "&sxsrf=ALeKk03MRCBotTPKt67gcfvlwd2mzg97Uw:1605097101396&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiK17vUvPrsAhWCBogKHVh2CEgQ_AUoAXoECAQQAw&cshid=1605097165583905&biw=724&bih=682"
+
+    def __init__(self, keyword):
+        self.keyword = quote_plus(keyword.strip())
+        self.url = self.BASE_URL + self.keyword + self.SEARCH_URL
+        self.images = {
+            "site": "google",
+            "keyword": keyword.strip(),
+            "src": [],
+        }
+
+    def __str__(self):
+        return self.keyword
+
+    def scrape(self):
+        with requests.Session() as session:
+            headers = {
+                "Referer": "https://www.google.com/",
+                "User-Agent": UserAgent().chrome,
+            }
+            response = session.get(self.url, headers=headers).text
+            soup = BeautifulSoup(response, "html.parser")
+            img_list = soup.select("div.isv-r > a > div > img")
+            if img_list:
+                for img in img_list:
+                    try:
+                        img_src = img["data-src"]
+                    except KeyError:
+                        pass
+                    else:
+                        self.images["src"].append(img_src)
+
+
 if __name__ == "__main__":
-    photo1 = NaverPhoto("미카미유아")
+    photo1 = GooglePhoto("조이")
     photo1.scrape()
     print(photo1.images)
